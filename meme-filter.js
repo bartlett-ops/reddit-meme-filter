f37d835aebe641c5b73aba8bbf3939a478881a26
Tom Bartlett <tbartlett@twig-world.com>
2019-02-04 12:19:14 +0000

    .

--- meme-filter.js
+++ meme-filter.js
@@ -1,3 +1,4 @@
 old.reddit.com##.unvoted.entry:has-text(/[^\w]+(three|3)[ ]*sea[ ]*shell(s)*[^\w]/i)
 old.reddit.com##.unvoted.entry:has-text(/[^\w]+trump[^\w]/i)
 old.reddit.com##.unvoted.entry:has-text(/[^\w]+Poem_for_your_sprog[^\w]/i)
+old.reddit.com##.unvoted.entry:has-text(/[^\w]+\( ͡° ͜ʖ ͡°\)[^\w]/i)
