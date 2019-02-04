9dc9d873d4944dba9486000a15a70fd34b235d78
Tom Bartlett <tbartlett@twig-world.com>
2019-02-04 13:03:30 +0000

    apply to all reddit subdomains

--- meme-filter.js
+++ meme-filter.js
@@ -1,4 +1,4 @@
-old.reddit.com##.unvoted.entry:has-text(/[^\w]+(three|3)[ ]*sea[ ]*shell(s)*[^\w]/i)
-old.reddit.com##.unvoted.entry:has-text(/[^\w]+trump[^\w]/i)
-old.reddit.com##.unvoted.entry:has-text(/[^\w]+Poem_for_your_sprog[^\w]/i)
-old.reddit.com##.unvoted.entry:has-text(/[^\w]+\(.* ͡.* ͡.*\)[^\w]/i)
+reddit.com##.unvoted.entry:has-text(/[^\w]+(three|3)[ ]*sea[ ]*shell(s)*[^\w]/i)
+reddit.com##.unvoted.entry:has-text(/[^\w]+trump[^\w]/i)
+reddit.com##.unvoted.entry:has-text(/[^\w]+Poem_for_your_sprog[^\w]/i)
+reddit.com##.unvoted.entry:has-text(/[^\w]+\(.* ͡.* ͡.*\)[^\w]/i)
